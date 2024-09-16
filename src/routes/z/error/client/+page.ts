a44ddbe3e393bd61fa8fb4f3b5226fd59107567c
Adam Shand <adam@shand.net>
2024-09-16 22:39:35 +1200

    .

--- src/routes/z/error/client/+page.ts
+++ src/routes/z/error/client/+page.ts
@@ -1,6 +1,6 @@
-export const ssr = false
-
 export const load = async () => {
 	// console.log(csrVariableNoWookie)
 	throw new Error('BOOM adam.nz/z/error/client BOOM')
 }
+
+export const ssr = false
