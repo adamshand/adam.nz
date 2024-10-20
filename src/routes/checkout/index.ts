bc2cb7c0cf6276e8c56c072359b5227411e65c6b
Adam Shand <adam@shand.net>
2024-10-21 12:13:29 +1300

    fix stripe build error (hopefully)

--- src/routes/checkout/index.ts
+++ src/routes/checkout/index.ts
@@ -1,5 +1,7 @@
-import Stripe from 'stripe'
+import { building } from '$app/environment'
 import { env } from '$env/dynamic/private'
+
+import Stripe from 'stripe'
 
 export interface Product {
 	id: number
@@ -11,7 +13,10 @@ export interface CartItem extends Product {
 	quantity: number
 }
 
-export const stripe = new Stripe(env.STRIPE_KEY)
+export let stripe: Stripe | undefined = undefined
+if (!building && env.STRIPE_KEY) {
+	stripe = new Stripe(env.STRIPE_KEY)
+}
 
 // export const stripe = new Stripe(env.STRIPE_KEY, {
 // 	apiVersion: '2023-08-16',
