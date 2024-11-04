d8d0c9a208f43515c953f8577281cc3d93ce2b8d
Adam Shand <adam@shand.net>
2024-11-05 09:53:31 +1300

    webstats only for non-admin users, add browser caching back

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte'
 	import { dev } from '$app/environment'
+
 	import Footer from '$lib/components/Footer.svelte'
 	import Header from '$lib/components/Header.svelte'
+
 	import '$lib/reset.css'
 	import '$lib/styles.css'
 	// import OpenProps from 'open-props'
 
-	let { children }: { children: Snippet } = $props()
+	let { children, data }: { children: Snippet; data } = $props()
+	$inspect(data)
 </script>
 
 <svelte:head>
@@ -15,11 +18,13 @@
 		<link href="https://cdn.jsdelivr.net/gh/hfg-gmuend/openmoji/color/svg/1F7E0.svg" rel="icon" />
 	{:else}
 		<link href="https://cdn.jsdelivr.net/gh/hfg-gmuend/openmoji/color/svg/2605.svg" rel="icon" />
-		<script
-			async
-			data-website-id="d5364df2-d71d-47b7-b39e-571c1ba77fda"
-			src="https://umami.haume.nz/script.js"
-		></script>
+		{#if !data.user?.admin}
+			<script
+				async
+				data-website-id="d5364df2-d71d-47b7-b39e-571c1ba77fda"
+				src="https://umami.haume.nz/script.js"
+			></script>
+		{/if}
 	{/if}
 </svelte:head>
 
@@ -47,6 +52,7 @@
 		width: calc(100vw - 2rem);
 	}
 	.dev {
-		border-top: 2px dashed goldenrod;
+		/* border-top: 2px dashed goldenrod; */
+		border-left: 3px dashed crimson;
 	}
 </style>
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,24 +4,24 @@ import { browser, dev } from '$app/environment'
 import { pbError } from '$lib/pocketbase.svelte.js'
 
 export const load: LayoutLoad = async ({ data, fetch }) => {
-	// if (browser && !dev) {
-	// 	const browserCache = sessionStorage.getItem('postsAndTypes')
+	if (browser && !dev) {
+		const browserCache = sessionStorage.getItem('postsAndTypes')
 
-	// 	if (browserCache) {
-	// 		const { posts, types } = JSON.parse(browserCache)
-	// 		console.log(`/+layout: sessionStorage posts (${posts.length}) and types (${types.length})`)
-	// 		return { ...data, posts, types }
-	// 	}
-	// }
+		if (browserCache) {
+			const { posts, types } = JSON.parse(browserCache)
+			console.log(`/+layout: sessionStorage posts (${posts.length}) and types (${types.length})`)
+			return { ...data, posts, types }
+		}
+	}
 
 	// console.log(`/+layout: (dev: ${dev}) sessionStorage disabled`)
 
 	try {
 		const { posts, types } = await fetch('/api/posts').then((r) => r.json())
 
-		// if (browser && !dev) {
-		// 	sessionStorage.setItem('postsAndTypes', JSON.stringify({ posts, types }))
-		// }
+		if (browser && !dev) {
+			sessionStorage.setItem('postsAndTypes', JSON.stringify({ posts, types }))
+		}
 
 		return { ...data, posts, types }
 	} catch (e) {
