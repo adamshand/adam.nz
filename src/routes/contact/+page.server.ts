e3fdc87b74992422e6eddb4d97997692316eadcd
Adam Shand <adam@shand.net>
2025-09-08 16:05:20 +1200

    /contact: update to pull text and image from main pb collection

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -1,6 +1,19 @@
-import { getRandomPbImageUrl } from '$lib/utils'
+// 
+import { pbAdamnzId } from '$lib/utils.js'
+import { pbError } from '$lib/pocketbase.svelte.js'
+import { getRandomElement } from '$lib'
 
-export const load = async ({ locals }) => {
-	const contactLols = 'kiochbanomkj7zi'
-	return await getRandomPbImageUrl(contactLols, '512x0')
+export const load = async ({ fetch, locals, url }) => {
+	// const contactLols = 'kiochbanomkj7zi'
+	// return await getRandomPbImageUrl(contactLols, '512x0')
+	try {
+		let post = await locals.pb.collection(pbAdamnzId).getFirstListItem('location="/contact"', {
+			fetch,
+			filter: `location = "${url.pathname}"`,
+		})
+		post.photos = [getRandomElement(post.photos)]
+		return { post }
+	} catch (e) {
+		pbError(e)
+	}
 }
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -3,10 +3,12 @@
 	import SocialIcons from '$lib/components/SocialIcons.svelte'
 
 	let { data } = $props()
-
-	const imageUrl = $derived(data.imageUrl)
-	const location = '/contact'
-	const title = 'Say hello?'
+	const imageUrl = $derived(
+		'https://pb.haume.nz/api/files/xd6xm0awdmqjjln/9o319tz40d3xxa5/' + data.post.photos[0],
+	)
+	$inspect({ post: data.post, imageUrl })
+	const location = data.post.location || ''
+	const title = data.post.title || ''
 	const description = `Thanks for stopping by. It's always nice to hear from visitors, if you are so inclined, please drop me a note and say hello.`
 </script>
 
@@ -14,17 +16,12 @@
 
 <section>
 	<div>
-		<img alt="" src={data.imageUrl} />
+		<img alt="" src={imageUrl} />
 	</div>
 
 	<div>
 		<h2>{title}</h2>
-		<p>Thanks for stopping by.</p>
-		<p>
-			It's always nice to hear from visitors. If something here caught your interest, I'd love to
-			hear about it.
-		</p>
-		<p>Or, just drop me a note and say hello. 👋🏻</p>
+		{@html data.post.content}
 		<SocialIcons />
 		<p>Cheers, <br />Adam.</p>
 	</div>
